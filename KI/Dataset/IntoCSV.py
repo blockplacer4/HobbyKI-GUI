363c36c4729f207d8feee62864d7c12d8182567d
blockplacer4 <99276350+blockplacer4@users.noreply.github.com>
2023-09-29 20:10:54 +0200

    Dataset 1.3

--- KI/Dataset/IntoCSV.py
+++ KI/Dataset/IntoCSV.py
@@ -2,8 +2,10 @@ import csv
 
 with open('D:\HobbyKI\HobbyKI-GUI\KI\Dataset\dataset.txt', 'r') as in_file, open('D:\HobbyKI\HobbyKI-GUI\KI\Dataset\dataset.csv', 'w', newline='') as out_file:
     writer = csv.writer(out_file)
-    writer.writerow(["Input", "Output"])
+    writer.writerow(["Input", "Output", "Text"])
 
     for line in in_file:
         input_part, output_part = line.replace('"', '').split(',', 1)
-        writer.writerow([input_part, output_part])
+        prompt = "Below is an request that provide a message or a question, paired with the input, write a response in german that appropriately answers the request.\n\n"
+        text_part = "### Prompt:\n" + prompt + "### Instruction:\n" + input_part + "\n\n" +  "### Response:\n" + output_part
+        writer.writerow([input_part, output_part, text_part])
