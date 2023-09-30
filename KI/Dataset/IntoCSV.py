84561083f598a71a38665587a4dad8f3f02f4f1a
blockplacer4 <99276350+blockplacer4@users.noreply.github.com>
2023-09-30 20:08:57 +0200

    3. Try

--- KI/Dataset/IntoCSV.py
+++ KI/Dataset/IntoCSV.py
@@ -6,6 +6,5 @@ with open('D:\HobbyKI\HobbyKI-GUI\KI\Dataset\dataset.txt', 'r') as in_file, open
 
     for line in in_file:
         input_part, output_part = line.replace('"', '').split(',', 1)
-        prompt = "Below is an request that provide a message or a question, paired with the input, write a response in german that appropriately answers the request.\n\n"
-        text_part = "### Prompt:\n" + prompt + "### Instruction:\n" + input_part + "\n\n" +  "### Response:\n" + output_part
+        text_part = "###Human:\n" + input_part + "\n\n" +  "###Assistant:\n" + output_part
         writer.writerow([input_part, output_part, text_part])
