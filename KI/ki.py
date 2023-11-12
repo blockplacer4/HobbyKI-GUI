2a49c6c12151b0b6b214fa768cf29ca30040109b
blockplacer4 <99276350+blockplacer4@users.noreply.github.com>
2023-11-12 01:03:50 +0100

    lol

--- KI/ki.py
+++ KI/ki.py
@@ -1,14 +1,14 @@
 import requests
+import json
 
-API_URL = "https://api-inference.huggingface.co/models/blockplacer4/Hobby-Ki-V6"
+API_URL = "https://api-inference.huggingface.co/models/blockplacer4/Hobby-Ki-V10"
 headers = {"Authorization": "Bearer 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"}
+prompt_input = "Was ist ein gutes Hobby?"
 
 def query(payload):
 	response = requests.post(API_URL, headers=headers, json=payload)
 	return response.json()
 	
-output = query({
-	"inputs": "Can you please let us know more details about your ",
-})
+output = query({"inputs": "Ich mag Pizza"})
 
 print(output)
