85add755e3a7698348e409db34eb62590dc0c14f
blockplacer4 <blockplacer4@gmail.com>
2024-01-18 21:26:51 +0100

    Update Flask.py

--- Web_GUI/Flask.py
+++ Web_GUI/Flask.py
@@ -1,14 +1,15 @@
 from flask import Flask, render_template, request, jsonify
-from deep_translator import GoogleTranslator
+from translate import Translator
 import os
 import replicate
 
 app = Flask(__name__)
 
 
-def translate(text):
-    translated = GoogleTranslator(source='auto', target='de').translate(text=text)
-    return translated
+def translate_text(texts):
+    translator = Translator(to_lang="de")
+    translation = translator.translate(texts)
+    return translation.text
 
 
 os.environ["REPLICATE_API_TOKEN"] = "r8_ELYcYXUjWpfDkGwboON58eCsBhK3p9x35KZ2U"
@@ -22,14 +23,34 @@ def home():
 @app.route('/get_bot_response', methods=['POST'])
 def get_bot_response():
     user_message = request.form['user_message']
-    bot_response = chat_with_ai(user_message)
-    resp = translate(bot_response)
-    return jsonify({'bot_response': resp})
+    bot_response = chat_second_ai(user_message)
+    translated = translate_text(bot_response)
+    print(translated)
+    print()
+    print(bot_response)
+    return jsonify({'bot_response': translated})
 
 
 def chat_with_ai(user_message):
-    return "Heyo"
-    # Secret Connection with Secret API Server <3
+    resp = ""
+    for event in replicate.stream(
+            "meta/llama-2-7b-chat",
+            input={"prompt": "bitte antworte in 3 **kurzen** Sätzen: " + user_message,
+                   "system_prompt": "Du bist ein hilfreicher Assistent, der konsequent die Vorteile des Programmierens als Hobby betont und immer in drei kurzen Sätzen antwortet. Dabei sorgst du stets für eine reibungslose Überleitung zum Thema 'Programmieren als Hobby'. Deine Antworten erfolgen immer auf Deutsch! Du antwortest immer in 3 kurzen Sätzen!",
+                   "max_new_tokens": 7},
+    ):
+        resp += str(event)
+    return resp
+
+
+def chat_second_ai(user_message):
+    output = replicate.run(
+        "meta/llama-2-7b-chat",
+        input={"prompt": user_message + " Bitte Anworte auf Deutsch!",
+               "system_prompt": "Du bist ein hilfreicher Assistent, der konsequent die Vorteile des Programmierens als Hobby betont und immer auf Deutsch antwortet. Dabei sorgst du stets für eine reibungslose Überleitung zum Thema 'Programmieren als Hobby'. Deine Antworten erfolgen immer auf Deutsch! Du antwortest immer in 3 kurzen Sätzen!",
+               "max_new_tokens": 800})
+    string_list = [str(item) for item in output]
+    return ''.join(string_list)
 
 
 if __name__ == '__main__':
